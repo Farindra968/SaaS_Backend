fb71f60cb1c21a2d2b3dc7afe2aeabfad483b616
Farindra <farindrabahadurbhandari@gmail.com>
2025-05-30 16:42:55 +0545

    Add environment configuration and database connection setup

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,7 +3,13 @@ import {config} from "dotenv";
 config()
 
 const envConfig = {
-    portNumber: process.env.PORT
+    portNumber: process.env.PORT,
+    databaseName: process.env.DB_NAME,
+    databaseUser: process.env.DB_USER,
+    databasePassword: process.env.DB_PASSWORD,
+    databaseHost: process.env.DB_HOST,
+    databasePort: process.env.DB_PORT,
+
 }
 
 
--- src/config/dbConnection.ts
+++ src/config/dbConnection.ts
@@ -1,1 +1,21 @@
 import { Sequelize } from "sequelize";
+import envConfig from "./config";
+
+const sequelize = new Sequelize({
+    database: envConfig.databaseName,
+    username: envConfig.databaseUser,
+    password: envConfig.databasePassword,
+    host: envConfig.databaseHost,
+    port: Number(envConfig.databasePort),
+    dialect: "mysql", // or 'postgres', 'sqlite', 'mariadb', 'mssql'
+});
+
+// Database connection configuration
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Database connection established successfully.");
+  })
+  .catch((error) => {
+    console.error("Unable to connect to the database:", error);
+  });

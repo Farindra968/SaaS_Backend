856ed5920b1e093394230ed86d7f60b1f67f18c1
Farindra <farindrabahadurbhandari@gmail.com>
2025-06-03 17:31:43 +0545

    Add confirmation password validation to registration process

--- src/controller/global/auth/auth.controller.ts
+++ src/controller/global/auth/auth.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import authService from '../../../services/global/auth/auth.service';
 
 const authRegister = async (req:Request, res:Response):Promise<void> => {
-    const {userName, email, password, phoneNumber}= req.body ;
+    const {userName, email, password, confirmpassword, phoneNumber}= req.body ;
     try {
         // Validate required fields
         if (!userName) { res.status(400).send('User name is required' );
@@ -16,6 +16,13 @@ const authRegister = async (req:Request, res:Response):Promise<void> => {
          }
         if (!phoneNumber) { res.status(400).send('Phone number is required'); 
             return;
+        }
+        if (!confirmpassword) { res.status(400).send('Confirm password is required');
+            return;
+        }
+        /// Check if password and confirm password match
+        if (password !== confirmpassword) {
+            res.status(400).send("Passwords do not match");
         }
 
         const data = await authService.authRegister(req.body);

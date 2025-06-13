b6495c0f3708a98c7ef844c1a30c1767535e111e
Farindra <farindrabahadurbhandari@gmail.com>
2025-06-13 21:38:16 +0545

    Add middleware class with isLoggedIn method skeleton

---
+++ src/middleware/middleware.ts
@@ -0,0 +1,9 @@
+import { NextFunction, Request, Response } from "express";
+
+class Middleware {
+    isLoggedIn(req:Request, res:Response, next:NextFunction) {
+
+    }
+}
+
+export default Middleware;

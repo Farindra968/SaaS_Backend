2ce6977a0c0b381663d71d638e8f8a2ebca393f5
Farindra <farindrabahadurbhandari@gmail.com>
2025-06-14 06:39:33 +0545

    Implement JWT verification in middleware; enhance error handling and user validation

--- src/middleware/middleware.ts
+++ src/middleware/middleware.ts
@@ -1,38 +1,44 @@
 import { NextFunction, Request, Response } from "express";
-import jwt from "jsonwebtoken";
-import envConfig from "../config/config";
 import User from "../models/user.model";
+import { verifyToken } from "../utils/jwttoken";
+
+interface IExtendRequest extends Request {
+  user?: {
+    id: string,
+    email: string;
+    role: string;
+    userName: string | null;
+  };
+}
 
 class Middleware {
   static async isLoggedIn(
-    req: Request,
+    req: IExtendRequest,
     res: Response,
     next: NextFunction
-  ): Promise<void> {
-    const token = req.headers.authorization;
-    console.log(`middleware:${token}`);
-    if (!token) {
-      res.status(401).send("Unauthorized Login");
-      return;
-    }
-    if (!envConfig.jsonSecret) {
-      res.status(400).send("No JSON SECRET Key");
-      return;
-    }
-    /// verifying jwt token
-    jwt.verify(token, envConfig.jsonSecret, async (error, data: any) => {
-      if (error) {
-        res.status(403).json("Invalid Token");
-      } else {
-        const userData = await User.findByPk(data.id);
+  ) {
+    const authToken = req.headers.authorization;
+    console.log(authToken);
+    // jwt verify
+    verifyToken(authToken) // src/utils/jwttoken.ts
+      // for success
+      .then(async (data: any) => {
+        // validing data from User Model
+        const userData = await User.findByPk(data);
+        console.log(`Userdata ${userData}`);
+        // if not valid
         if (!userData) {
-          res.status(403).json({ message: "No User Found with this token" });
+          res.status(403).json({ message: "No User found" });
+          return;
         } else {
-          console.log(`UserData= ${data.userName}, ${data.email}`);
+          req.user = userData;
           next();
         }
-      }
-    });
+      })
+      // for error
+      .catch(() => {
+        res.status(401).send("Invalid Token");
+      });
   }
 }
 
--- src/utils/jwttoken.ts
+++ src/utils/jwttoken.ts
@@ -20,5 +20,22 @@ const jsonToken = (data: ITokenData) => {
   return token;
 };
 
+const verifyToken = async(authToken:any)=>{
+  return await new Promise((reject, resolve)=>{
+    // if Secret key not found
+    if(!envConfig.jsonSecret) {
+      throw {
+        statusCode: 500,
+      message: "JSON secret is not defined in environment variables",
+    };
+    } 
+    jwt.verify(authToken, envConfig.jsonSecret, (error:any, data:any)=>{
+      if(error) return reject;
+      return resolve(data)
 
-export { jsonToken };
+    })
+  })
+}
+
+
+export { jsonToken, verifyToken };
